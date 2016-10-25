76e17ea616911f889fc105da9065b8a2a3dc28a5
Leo <leolopelofranco@gmail.com>
2016-10-25 15:37:03 +0800

    removed fb functions

--- app/assets/javascripts/angular/controllers/home_controller.js
+++ app/assets/javascripts/angular/controllers/home_controller.js
@@ -5,65 +5,18 @@ angular.module('Hive.controllers')
     $scope.passcode = false
     $scope.fb = true
 
-    getPicture = function(token) {
-      Facebook.api('/me', 'GET', {fields: 'name, picture.height(50).width(50)', access_token: token}, function(r) {
-        data = {
-          "id": r.id,
-          "name": r.name,
-          "picture": r.picture.data.url
-        }
-        return data
-      });
-    }
-
-
-    role = function(token) {
-      Facebook.api('/me', 'GET', {fields: 'accounts.limit(100)', access_token: token}, function(response) {
-        pages = response.accounts.data
-
-        _.each(pages, function(a){
-          if(a.id== '1779236905645324') {
-            if(_.contains(a.perms, 'ADMINISTER')) {
-              return a
-            }
-          }
-        });
-      });
-    }
-
-    getLoginStatus = function() {
-      Facebook.getLoginStatus(function(response) {
-        console.log(response)
-        if(response.status === 'connected') {
-          $scope.loggedIn = true;
-          picture_data =  getPicture(response.authResponse.accessToken)
-          page = role(response.authResponse.accessToken)
-          $scope.passcode = true
-          $scope.fb = false
-        } else {
-          $scope.loggedIn = false;
-        }
-      });
-     }
-
-    $scope.fblogin = function() {
-    //  From now on you can use the Facebook service just as Facebook api says
-     Facebook.login(function(response) {
-
-       $rootScope.token = response.authResponse.accessToken
-
-       if(response.status === 'connected') {
-         picture_data = getPicture(response.authResponse.accessToken)
-         page = role(response.authResponse.accessToken)
-       }
-     });
-   };
-
-   getLoginStatus()
-
 
    $scope.submitLogin = function() {
      console.log($scope.user.password)
+     result = {
+       page_id: '1779236905645324',
+       type: 'honeypot_v0',
+       status: 'success'
+     }
+
+     if(result.type == 'honeypot_v0') {
+       $window.open('https://damp-escarpment-77884.herokuapp.com/analytics/basic', '_self')
+     }
    }
 
 
