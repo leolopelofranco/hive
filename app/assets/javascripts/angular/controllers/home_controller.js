af892ba79ec54927a12be26d0fb80eee97ab5a18
Leo <leolopelofranco@gmail.com>
2016-10-25 15:06:41 +0800

    changed placing

--- app/assets/javascripts/angular/controllers/home_controller.js
+++ app/assets/javascripts/angular/controllers/home_controller.js
@@ -5,6 +5,47 @@ angular.module('Hive.controllers')
     $scope.passcode = false
     $scope.fb = true
 
+    getLoginStatus = function() {
+      Facebook.getLoginStatus(function(response) {
+        console.log(response)
+        if(response.status === 'connected') {
+          $scope.loggedIn = true;
+          picture_data =  getpicture(response.authResponse.accessToken)
+          page = role(response.authResponse.accessToken)
+          $scope.passcode = true
+          $scope.fb = false
+        } else {
+          $scope.loggedIn = false;
+        }
+      });
+     }
+
+     getPicture = function(token) {
+       Facebook.api('/me', 'GET', {fields: 'name, picture.height(50).width(50)', access_token: token}, function(r) {
+         data = {
+           "id": r.id,
+           "name": r.name,
+           "picture": r.picture.data.url
+         }
+         return data
+       });
+     }
+
+
+     role = function(token) {
+       Facebook.api('/me', 'GET', {fields: 'accounts.limit(100)', access_token: token}, function(response) {
+         pages = response.accounts.data
+
+         _.each(pages, function(a){
+           if(a.id== '1779236905645324') {
+             if(_.contains(a.perms, 'ADMINISTER')) {
+               return a
+             }
+           }
+         });
+       });
+     }
+
 
     $scope.fblogin = function() {
     //  From now on you can use the Facebook service just as Facebook api says
@@ -18,47 +59,6 @@ angular.module('Hive.controllers')
        }
      });
    };
-
-   getLoginStatus = function() {
-     Facebook.getLoginStatus(function(response) {
-       console.log(response)
-       if(response.status === 'connected') {
-         $scope.loggedIn = true;
-         picture_data =  getpicture(response.authResponse.accessToken)
-         page = role(response.authResponse.accessToken)
-         $scope.passcode = true
-         $scope.fb = false
-       } else {
-         $scope.loggedIn = false;
-       }
-     });
-    }
-
-    getPicture = function(token) {
-      Facebook.api('/me', 'GET', {fields: 'name, picture.height(50).width(50)', access_token: token}, function(r) {
-        data = {
-          "id": r.id,
-          "name": r.name,
-          "picture": r.picture.data.url
-        }
-        return data
-      });
-    }
-
-
-    role = function(token) {
-      Facebook.api('/me', 'GET', {fields: 'accounts.limit(100)', access_token: token}, function(response) {
-        pages = response.accounts.data
-
-        _.each(pages, function(a){
-          if(a.id== '1779236905645324') {
-            if(_.contains(a.perms, 'ADMINISTER')) {
-              return a
-            }
-          }
-        });
-      });
-    }
 
    getLoginStatus()
 
