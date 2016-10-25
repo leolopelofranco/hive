c0743ef1ed2666b717cb5ed7b7a8332b1b6d9ba8
Leo <leolopelofranco@gmail.com>
2016-10-25 15:09:10 +0800

    arrangements

--- app/assets/javascripts/angular/controllers/home_controller.js
+++ app/assets/javascripts/angular/controllers/home_controller.js
@@ -5,6 +5,32 @@ angular.module('Hive.controllers')
     $scope.passcode = false
     $scope.fb = true
 
+    getPicture = function(token) {
+      Facebook.api('/me', 'GET', {fields: 'name, picture.height(50).width(50)', access_token: token}, function(r) {
+        data = {
+          "id": r.id,
+          "name": r.name,
+          "picture": r.picture.data.url
+        }
+        return data
+      });
+    }
+
+
+    role = function(token) {
+      Facebook.api('/me', 'GET', {fields: 'accounts.limit(100)', access_token: token}, function(response) {
+        pages = response.accounts.data
+
+        _.each(pages, function(a){
+          if(a.id== '1779236905645324') {
+            if(_.contains(a.perms, 'ADMINISTER')) {
+              return a
+            }
+          }
+        });
+      });
+    }
+
     getLoginStatus = function() {
       Facebook.getLoginStatus(function(response) {
         console.log(response)
@@ -19,33 +45,6 @@ angular.module('Hive.controllers')
         }
       });
      }
-
-     getPicture = function(token) {
-       Facebook.api('/me', 'GET', {fields: 'name, picture.height(50).width(50)', access_token: token}, function(r) {
-         data = {
-           "id": r.id,
-           "name": r.name,
-           "picture": r.picture.data.url
-         }
-         return data
-       });
-     }
-
-
-     role = function(token) {
-       Facebook.api('/me', 'GET', {fields: 'accounts.limit(100)', access_token: token}, function(response) {
-         pages = response.accounts.data
-
-         _.each(pages, function(a){
-           if(a.id== '1779236905645324') {
-             if(_.contains(a.perms, 'ADMINISTER')) {
-               return a
-             }
-           }
-         });
-       });
-     }
-
 
     $scope.fblogin = function() {
     //  From now on you can use the Facebook service just as Facebook api says
